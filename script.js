26c7ee689e85c344f6e144e54172279acc026fdf
Morgan Grage Rasmussen <morgangr01@gmail.com>
2019-11-15 12:42:26 +0100

    svg editted

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let battles = [];
 
 loadJSON();
 loadJSONcharaters();
+fetchSVG();
 
 function loadJSON() {
   fetch("timeline.json")
@@ -20,3 +21,22 @@ function loadJSONcharaters() {
       console.log(jsonDataChar);
     });
 }
+
+function fetchSVG() {
+  fetch("svg/timeline.svg")
+    .then(response => response.text())
+    .then(svg => {
+      document.querySelector("#timeline").innerHTML = svg;
+      clickBattle();
+    });
+}
+
+function clickBattle() {
+  document.querySelector("#battle1").addEventListener("click", test);
+}
+
+function test() {
+  console.log("test");
+}
+
+function showData() {}
