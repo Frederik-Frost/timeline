23e46f81d5cdbea55a17fc236134060ce20f251c
=Frederik <=fred84e0@stud.kea.dk>
2019-11-18 11:42:20 +0100

    map works

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let battles = [];
 loadJSON();
 loadJSONcharaters();
 fetchSVG();
+fetchMap();
 
 function loadJSON() {
   fetch("timeline.json")
@@ -32,11 +33,43 @@ function fetchSVG() {
 }
 
 function clickBattle() {
-  document.querySelector("#battle1").addEventListener("click", test);
+  document.querySelector(".battles").addEventListener("click", getV);
 }
 
-function test() {
+function getV() {
   console.log("test");
+  const target = event.target.id.slice(6);
+  console.log(target);
+
+  test(target);
+}
+
+function test(target) {
+  document.querySelector(`#b${target}`).classList.toggle("show-location");
+  let bboxCircle = document.querySelector(`#b${target}`).getBBox();
+  console.log(bboxCircle.y);
+
+  let scaler1 = document.querySelector(`#b${target}`).getAttribute("cx");
+  let scaler2 = document.querySelector(`#b${target}`).getAttribute("cy");
+  console.log(scaler1, scaler2);
+
+  let calcX = Math.round(scaler1 / 39);
+  let calcY = Math.round(scaler2 / 29);
+
+  console.log(calcX);
+  console.log(calcY);
+
+  document.querySelector("#map svg").style.transformOrigin = `${calcX}% ${calcY}%`;
+  document.querySelector("#map svg").classList.toggle("zoomin");
 }
 
 function showData() {}
+
+function fetchMap() {
+  fetch("svg/map.svg")
+    .then(response => response.text())
+    .then(map => {
+      document.querySelector("#map").innerHTML = map;
+      clickBattle();
+    });
+}
