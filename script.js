4b7813fe2e333567923fe0c5a63fe9f2c803a19f
Chriss <=>
2019-11-18 13:01:42 +0100

    minimalt

--- script.js
+++ script.js
@@ -21,7 +21,6 @@ function loadJSONcharaters() {
     .then(jsonDataChar => {
       console.log(jsonDataChar);
       console.log(jsonDataChar.characters[1].name);
-      // first();
       let src = jsonDataChar.characters;
       document.querySelector("#battle1").addEventListener("click", b1);
       document.querySelector("#battle2").addEventListener("click", b2);
@@ -38,57 +37,59 @@ function loadJSONcharaters() {
         bad.innerHTML = "";
         good.innerHTML = `<img class="svg" src=svg/${src[7].img} />`;
         bad.innerHTML = `<img class="svg" src=svg/${src[10].img} />`;
-        document.querySelectorAll(".svg").forEach(em => {
-          em.classList.add("icon");
-        });
+        setTimeout(function() {
+          document.querySelectorAll(".svg").forEach(em => {
+            em.classList.add("icon");
+          });
+        }, 500);
       }
       function b2() {
         good.innerHTML = "";
         bad.innerHTML = "";
         good.innerHTML = `<img class="svg" src=svg/${src[2].img} />`;
-        bad.innerHTML = `<img class="svg" src=svg/${src[9].img} />`;
+        bad.innerHTML = `<img class="svg right" src=svg/${src[9].img} />`;
       }
       function b3() {
         good.innerHTML = "";
         bad.innerHTML = "";
         good.innerHTML = `<img class="svg" src=svg/${src[5].img} />`;
-        bad.innerHTML = `<img class="svg" src=svg/${src[14].img} />`;
+        bad.innerHTML = `<img class="svg right" src=svg/${src[14].img} />`;
       }
       function b4() {
         good.innerHTML = "";
         bad.innerHTML = "";
         good.innerHTML = `<img class="svg" src=svg/${src[0].img} /> <img class="svg" src=svg/${src[1].img} /> <img class="svg" src=svg/${src[2].img} /> <img class="svg" src=svg/${src[3].img} /> <img class="svg" src=svg/${src[4].img} /> <img class="svg" src=svg/${src[5].img} /> <img class="svg" src=svg/${src[6].img} />`;
-        bad.innerHTML = `<img class="svg" src=svg/${src[12].img} /> <img class="svg" src=svg/${src[15].img} /> <img class="svg" src=svg/${src[17].img} />`;
+        bad.innerHTML = `<img class="svg right" src=svg/${src[12].img} /> <img class="svg right" src=svg/${src[15].img} /> <img class="svg right" src=svg/${src[17].img} />`;
       }
       function b5() {
         good.innerHTML = "";
         bad.innerHTML = "";
         good.innerHTML = `<img class="svg" src=svg/${src[0].img} /> <img class="svg" src=svg/${src[1].img} /> <img class="svg" src=svg/${src[2].img} /> <img class="svg" src=svg/${src[3].img} /> <img class="svg" src=svg/${src[4].img} /> <img class="svg" src=svg/${src[5].img} /> <img class="svg" src=svg/${src[6].img} />`;
-        bad.innerHTML = `<img class="svg" src=svg/${src[13].img} /> `;
+        bad.innerHTML = `<img class="svg right" src=svg/${src[13].img} /> `;
       }
       function b6() {
         good.innerHTML = "";
         bad.innerHTML = "";
         good.innerHTML = `<img class="svg" src=svg/${src[7].img} />`;
-        bad.innerHTML = `<img class="svg" src=svg/${src[13].img} /><img class="svg" src=svg/${src[12].img} />  `;
+        bad.innerHTML = `<img class="svg right" src=svg/${src[13].img} /><img class="svg right" src=svg/${src[12].img} />  `;
       }
       function b7() {
         good.innerHTML = "";
         bad.innerHTML = "";
         good.innerHTML = `<img class="svg" src=svg/${src[7].img} /> <img class="svg" src=svg/${src[4].img} /> <img class="svg" src=svg/${src[3].img} /> <img class="svg" src=svg/${src[5].img} />`;
-        bad.innerHTML = `<img class="svg" src=svg/${src[13].img} /><img class="svg" src=svg/${src[12].img} />  `;
+        bad.innerHTML = `<img class="svg right" src=svg/${src[13].img} /><img class="svg right" src=svg/${src[12].img} />  `;
       }
       function b8() {
         good.innerHTML = "";
         bad.innerHTML = "";
         good.innerHTML = `<img class="svg" src=svg/${src[7].img} /> <img class="svg" src=svg/${src[4].img} /> <img class="svg" src=svg/${src[3].img} /> <img class="svg" src=svg/${src[5].img} />`;
-        bad.innerHTML = `<img class="svg" src=svg/${src[13].img} /><img class="svg" src=svg/${src[12].img} />  `;
+        bad.innerHTML = `<img class="svg right" src=svg/${src[13].img} /><img class="svg right" src=svg/${src[12].img} />  `;
       }
       function b9() {
         good.innerHTML = "";
         bad.innerHTML = "";
         good.innerHTML = `<img class="svg" src=svg/${src[18].img} />`;
-        bad.innerHTML = `<img class="svg" src=svg/${src[13].img} /><img class="svg" src=svg/${src[12].img} />  `;
+        bad.innerHTML = `<img class="svg right" src=svg/${src[13].img} /><img class="svg right" src=svg/${src[12].img} />  `;
       }
     });
 }
